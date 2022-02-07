69e1a3d0e452f9cab828bbd39c4d61708cba057c
Aung Myo Khant <aungmyokhant2465@gmail.com>
2022-02-07 20:22:58 +0630

    02-07-2022

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import moment from 'moment';
 import GIF from './images/GIF.gif';
 import Slider2 from './Slider2';
 import Announce from './Announce';
+import Image1 from './images/image1.png';import {
+  CSSTransition,
+  TransitionGroup,
+} from 'react-transition-group';
 
 const TRIPS = gql`
 query MyQuery ($id: Int!, $nowDate: timestamptz) {
@@ -36,33 +40,53 @@ query MyQuery {
 
 const App = () => {
 
-  const [loadTrips , resultTrips] = useLazyQuery(TRIPS)
+  const [loadTrips , resultTrips] = useLazyQuery(TRIPS, {
+    fetchPolicy: 'network-only',
+    notifyOnNetworkStatusChange: true,
+    // pollInterval: 5000
+  })
+  const [loadTripsAgain, resultTripsAgain] = useLazyQuery(TRIPS, {
+    fetchPolicy: 'network-only',
+    notifyOnNetworkStatusChange: true,
+    // pollInterval: 5000
+  })
   const result = useQuery(TRANSPORTTERMINALSNAME)
 
   const [ trips, setTrips ] = useState(null)
-  const [ terminal, setTerminal ] = useState({})
+  const [ terminal, setTerminal ] = useState(null)
   const [ inter, setInter ] = useState(null)
+  const [ selected, setSelected ] = useState(false)
 
   useEffect(() => {
     if(resultTrips.data) {
       setTrips(resultTrips.data.ptp_trips)
-    }
-  }, [resultTrips.data])
-
-  // let inter
-  useEffect(() => {
-    if(trips && trips.length > 0) {
-      const inte = setInterval(() => {
-        loadTrips({ variables: { id: terminal.transport_terminal_id, nowDate: new Date() }})
-      }, (1000 * 60 * 5));
-      setInter(inte)
-    } else {
-      clearInterval(inter)
+      setSelected(true)
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [trips])
+  }, [resultTrips])
 
-  if(result.loading || resultTrips.loading) {
+  useEffect(() => {
+    if(resultTripsAgain.data) {
+      console.log('hi')
+      setTrips(resultTripsAgain.data.ptp_trips)
+    }
+  }, [resultTripsAgain.data])
+
+  useEffect(() => {
+    if(selected) {
+      console.log('start')
+      const inte = setInterval(() => {
+        let date = new Date()
+        // loadTrips({ variables: { id: terminal.transport_terminal_id, nowDate: new Date('2022-02-19T03:24:00') }})
+        // console.log(her)
+        resultTrips.refetch({ id: terminal.transport_terminal_id, nowDate: date })
+      }, (1000 * 5));
+      // setInter(inte)
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [selected])
+
+  if(result.loading) {
     return <div>Loading...</div>
   }
 
@@ -77,12 +101,13 @@ const App = () => {
 
   const handleBack = () => {
     setTrips(null)
+    setTerminal(null)
   }
 
   return (
     <div className='container'>
       {
-        (!trips) && (
+        (!trips && !terminal) && (
           <div className='form-group' >
             <label htmlFor='terminal' >Select Start Terminal</label>
             <select id='terminal' name='terminal' onChange={handleChange}>
@@ -105,23 +130,26 @@ const App = () => {
         )
       }
       {
-        ( trips && trips.length > 0) && 
+        (terminal) &&
         <>
           <main>
             <table id="conta">
               <thead>
                 <tr>
                   <th id="imgContainer" rowSpan='2' ><img src={GIF} id="logoGIF" alt='oro logo'/></th>
-                  <th colSpan='2'>STA. ROSA INTEGRATED TERMINAL</th>
-                  <th rowSpan='2' id='clock-container'><Clock format={'HH:mm:ss'} ticking={true} /></th>
+                  <th colSpan='2' className='header'>STA. ROSA INTEGRATED TERMINAL</th>
+                  <th rowSpan='2' id='clock-container'>
+                    {moment().format('LL')} <br/>
+                    <Clock format={'HH:mm:ss'} ticking={true} />
+                  </th>
                 </tr>
                 <tr>
-                  <th colSpan='2'>SRIT BUS SCHEDULES</th>
+                  <th colSpan='2' className='header'>SRIT BUS SCHEDULES</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
-                  <td colSpan='3'>
+                  <td colSpan='3' rowSpan='2'>
                     <table id='inner'>
                       <thead>
                         <tr>
@@ -131,24 +159,41 @@ const App = () => {
                           <th>PLATE NUMBER</th>
                         </tr>
                       </thead>
-                      <tbody>
+                      {
+                      ( trips && trips.length > 0) && (
+                      // <tbody>
+                      <TransitionGroup component="tbody" className="todo-list">
                         {
                           trips.map((t, index) => (
-                            <tr key={index} >
-                              <td>{t.ptp_route.end_terminal?.transport_terminal_name}</td>
-                              <td>{moment(t.depart_time).format('LT')}</td>
-                              <td></td>
-                              <td>{t.vehicle_account?.vehicle_plate_number}</td>
-                            </tr>
+                            <CSSTransition
+                              // in={!!trips.length}
+                              key={index}
+                              timeout={2000}
+                              classNames="item"
+                            >
+                              <tr>
+                                <td>{t.ptp_route.end_terminal?.transport_terminal_name}</td>
+                                <td>{moment(t.depart_time).format('LT')}</td>
+                                <td></td>
+                                <td>{t.vehicle_account?.vehicle_plate_number}</td>
+                              </tr>
+                            </CSSTransition>
                           ))
                         }
-                      </tbody>
+                      </TransitionGroup>
+                      // </tbody>
+                      )
+                      }
+                      
                     </table>
                   </td>
                   <td>
                     {/* <Slider /> */}
-                    <Slider2 />
+                    <img src={Image1} style={{width:'100%'}} alt="" />
                   </td>
+                </tr>
+                <tr>
+                  <td><Slider2 /></td>
                 </tr>
               </tbody>
               <tfoot>
